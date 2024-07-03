3a122a9fa1b6e379f9f854af97f219eef0de1b8f
Matthew13483 <manzanaresmatthew0@gmail.com>
2024-07-03 12:03:44 +0800

    update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ const fps = {
 		let time = Date.now() - fps.startTime;
 		if (time > 1000 / 5) {
 			if (fps.visible) fps_display.innerHTML = 'FPS: ' + (1000 * fps.frameCount / time).toFixed(2);
-			console.log(timeA / frames);
+			//console.log(timeA / frames);
 			timeA = 0;
 			frames = 0;
 			fps.startTime = Date.now();
