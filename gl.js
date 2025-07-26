eb0d283c0a3274cf9ca17915fdf8fd8b9901e31a
Matthew13483 <manzanaresmatthew0@gmail.com>
2025-07-27 06:12:54 +0800

    update performance opt 8

--- gl.js
+++ gl.js
@@ -115,16 +115,11 @@ function GL(canvas) {
 		gl.attachShader(program, vertexShader);
 		gl.attachShader(program, fragmentShader);
 		gl.linkProgram(program);
-		let success = gl.getProgramParameter(program, gl.LINK_STATUS);
-		if (success) return program;
-
-		console.log(gl.getProgramInfoLog(program));
-		gl.deleteProgram(program);
+		return program;
 	};
 
 	let program = createProgram(gl, vertexShader, fragmentShader);
 	gl.useProgram(program);
-
 	gl.program = program;
 
 	return gl;
--- voronoi.js
+++ voronoi.js
@@ -83,11 +83,7 @@ class Voronoi {
 			gl.attachShader(program, vertexShader);
 			gl.attachShader(program, fragmentShader);
 			gl.linkProgram(program);
-			let success = gl.getProgramParameter(program, gl.LINK_STATUS);
-			if (success) return program;
-
-			console.log(gl.getProgramInfoLog(program));
-			gl.deleteProgram(program);
+			return program;
 		};
 
 		let program = createProgram(gl, vertexShader, fragmentShader);
