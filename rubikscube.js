e42793eee75fceed7955e002aa00bf77d961d4a9
Matthew13483 <manzanaresmatthew0@gmail.com>
2025-07-28 01:32:46 +0800

    update performance opt 9

--- rubikscube.js
+++ rubikscube.js
@@ -138,7 +138,130 @@ class RubiksCube {
 	}
 
 	init_gl() {
-		this.gl = GL(this.canvas);
+		let gl = canvas.getContext("webgl", { alpha: true });
+		if (!gl) {
+			console.error('WebGL is not supported');
+			return;
+		}
+		
+		let createShader = (gl, type, source) => {
+			let shader = gl.createShader(type);
+			gl.shaderSource(shader, source);
+			gl.compileShader(shader);
+			let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+			if (success) return shader;
+			
+			console.log(gl.getShaderInfoLog(shader));
+			gl.deleteShader(shader);
+		};
+		
+		let vertexShaderSource = `
+				attribute vec3 a_position;
+				attribute vec3 a_normal;
+				attribute vec3 a_color;
+				attribute float a_id;
+				attribute float a_type;
+				
+				uniform vec2 u_resolution;
+				uniform mat4 u_iMat[27];
+				uniform mat4 u_pMat;
+				uniform mat4 u_mMat;
+				uniform mat4 u_vMat;
+		
+				varying vec3 v_position;
+				varying vec3 v_color;
+				varying vec3 v_normal;
+				varying float v_type;
+				
+				void main() {
+					vec4 worldPos = u_mMat * u_iMat[int(a_id)] * vec4(a_position, 1.0);
+					gl_Position = u_pMat * u_vMat * worldPos;
+					v_position = vec3(worldPos);
+					v_normal = mat3(u_mMat) * mat3(u_iMat[int(a_id)]) * a_normal;
+					v_color = a_color;
+					v_type = a_type;
+				}
+			`;
+		let fragmentShaderSource = `
+				precision mediump float;
+				uniform vec3 camPos;
+				
+				uniform vec3 lightSources[5];
+				uniform vec3 lightColors[5];
+				
+				uniform sampler2D u_image;
+				
+				varying vec3 v_position;
+				varying vec3 v_color;
+				varying vec3 v_normal;
+				varying float v_type;
+				
+				void main() {
+					vec2 pos = gl_FragCoord.xy;
+					
+					vec3 color = v_color;
+					float alpha = 1.0;
+					float diff_mul = 1.0;
+					float spec_mul = 1.0;
+					
+					if (v_type == -1.0) {
+						color = texture2D(u_image, v_color.xy).xyz;
+						alpha = texture2D(u_image, v_color.xy).a;
+					}
+					if (v_type == 0.0) {
+						diff_mul = 0.9;
+						spec_mul = 0.1;
+					}
+					
+					float len = length(v_position);
+					diff_mul = min(diff_mul * 0.444 * len * len, diff_mul);
+					
+					vec3 ambient = 0.1 * color + 0.3 * vec3(24.0, 24.0, 48.0) / 255.0;
+					vec3 diffuse = vec3(0.0);
+					vec3 specular = vec3(0.0);
+					
+					for (int i = 0; i < 5; i++) {
+						vec3 lightPos = lightSources[i];
+						
+						vec3 norm = normalize(v_normal);
+						vec3 lightDir = normalize(lightPos - v_position);
+						float diff = max(dot(norm, lightDir), 0.0);
+						diffuse += lightColors[i] * diff * color;
+						
+						vec3 viewDir = normalize(camPos - v_position);
+						vec3 reflectDir = reflect(-lightDir, norm);
+						float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+						specular += lightColors[i] * spec;
+					}
+					diffuse = clamp(diffuse, 0.0, 1.0);
+					specular = clamp(specular, 0.0, 1.0);
+					
+					vec3 result = ambient + diff_mul * diffuse + 0.5 * spec_mul * specular;
+					
+					vec3 shade0 = 0.8 * v_color;
+					vec3 shade1 = v_color * vec3(dot(v_normal, normalize(lightSources[0] - v_position))) * 0.3;
+					vec3 result2 = shade0 + shade1;
+					
+					gl_FragColor = vec4(result, alpha);
+				}
+			`;
+		
+		let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+		let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+		
+		let createProgram = (gl, vertexShader, fragmentShader) => {
+			let program = gl.createProgram();
+			gl.attachShader(program, vertexShader);
+			gl.attachShader(program, fragmentShader);
+			gl.linkProgram(program);
+			return program;
+		};
+		
+		let program = createProgram(gl, vertexShader, fragmentShader);
+		gl.useProgram(program);
+		gl.program = program;
+		
+		this.gl = gl;
 	}
 
 	reset() {
@@ -201,7 +324,10 @@ class RubiksCube {
 	resize() {
 		this.display.width = this.canvas.width;
 		this.display.height = this.canvas.height;
-		GLresize(this.gl);
+		if (this.gl) {
+			this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+			this.gl.uniform2f(this.gl.getUniformLocation(this.gl.program, 'u_resolution'), this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+		}
 	}
 
 	loop() {
@@ -636,15 +762,19 @@ Rubik.turnAlgInstant(Rubik.solves[0].solution);
 		gl.enable(gl.BLEND);
 		gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-		let texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, logo);
+		this.logo = new Image();
+		this.logo.src = "logo.svg";
+		this.logo.onload = () => {
+			let texture = gl.createTexture();
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.logo);
+		};
 		
 		let lightSources = [
 			0, 10, 0,
